0c98ca2a4cbd060a55736a3e9567095b95398e26
johanna <johi77a@gmail.com>
2022-10-22 03:51:23 +0200

    ingreso-de-main-y-acerca-de-mi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { AcercaDeMiComponent } from './components/main/acerca-de-mi/acerca-de-mi.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeaderComponent
+    HeaderComponent,
+    MainComponent,
+    AcercaDeMiComponent
   ],
   imports: [
     BrowserModule,
